0dd6c76282e784cb13ab34e28e09abcecec5817b
chemodax <chemodax@gmail.com>
2026-05-15 14:34:01 +0200

    Simplify code a bit.

--- EditorConfigGuidelines/Guideline.cs
+++ EditorConfigGuidelines/Guideline.cs
@@ -4,16 +4,13 @@ namespace EditorConfigGuidelines
 {
     internal class Guideline
     {
-        private readonly int column;
-        private readonly DoubleCollection? dashArray;
-
         public Guideline(int column, DoubleCollection? dashArray)
         {
-            this.column = column;
-            this.dashArray = dashArray;
+            Column = column;
+            DashArray = dashArray;
         }
 
-        public int Column { get { return column; } }
-        public DoubleCollection? DashArray { get { return dashArray; } }
+        public int Column { get; }
+        public DoubleCollection? DashArray { get;  }
     }
 }
